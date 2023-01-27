e26e106884f7628be2e6a96e542074b6233c5bbd
Lorna Evans <LornaSIL@users.noreply.github.com>
2023-01-27 10:27:50 -0600

    Update documentation file and copyright dates.

--- documentation/source/makepsmd.py
+++ documentation/source/makepsmd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''Process font documentation .md files for use on product sites '''
 __url__ = 'http://github.com/silnrsi/fontdocs'
-__copyright__ = 'Copyright (c) 2021 SIL International (http://www.sil.org)'
+__copyright__ = 'Copyright (c) 2021-2023 SIL International (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
 __author__ = 'Victor Gaultney'
 
@@ -63,14 +63,29 @@ def doit(args):
     # replace links to external markdown files
     temptext = temptext.replace(".rawmd",".md")
 
+    # replace escaped @ in link texts
+    temptext = temptext.replace("[\@","[@")
+
     # make a list of new classes needed for font shortcode
-    fontclass = re.compile(r"style='font-feature-settings:\s\"(\w+\d*)\"\s(\d+)'")
-    for match in fontclass.finditer(temptext):
+    # separate searches for spans with 1, 2, or 3 settings
+    fontclass1 = re.compile(r"style='font-feature-settings:\s\"(\w+\d*)\"\s(\d+)'")
+    fontclass2 = re.compile(r"style='font-feature-settings:\s\"(\w+\d*)\"\s(\d+),\s\"(\w+\d*)\"\s(\d+)'")
+    fontclass3 = re.compile(r"style='font-feature-settings:\s\"(\w+\d*)\"\s(\d+),\s\"(\w+\d*)\"\s(\d+),\s\"(\w+\d*)\"\s(\d+)'")
+    for match in fontclass1.finditer(temptext):
          classes.append(("-" + match.group(1) + "-" + match.group(2), match.group(1) + " " + match.group(2)))
+    for match in fontclass2.finditer(temptext):
+         classes.append(("-" + match.group(1) + "-" + match.group(2) + "-" + match.group(3) + "-" + match.group(4), match.group(1) + " " + match.group(2) + ", " + match.group(3) + " " + match.group(4)))
+    for match in fontclass3.finditer(temptext):
+         classes.append(("-" + match.group(1) + "-" + match.group(2) + "-" + match.group(3) + "-" + match.group(4) + "-" + match.group(5) + "-" + match.group(6), match.group(1) + " " + match.group(2) + ", " + match.group(3) + " " + match.group(4) + ", " + match.group(5) + " " + match.group(6)))
 
     # transform explicit font feature settings into classes
-    fontspan = re.compile(r"class='(\w+)+-(\w+)+\snormal'\sstyle='font-feature-settings:\s\"(\w+\d*)\"\s(\d+)'>")
-    temptext = fontspan.sub(r"class='\1-\3-\4-\2 normal'>", temptext)
+    # separate searches for spans with 1, 2, or 3 settings
+    fontspan1 = re.compile(r"class='(\w+)+-(\w+)+\snormal'\sstyle='font-feature-settings:\s\"(\w+\d*)\"\s(\d+)'>")
+    fontspan2 = re.compile(r"class='(\w+)+-(\w+)+\snormal'\sstyle='font-feature-settings:\s\"(\w+\d*)\"\s(\d+),\s\"(\w+\d*)\"\s(\d+)'>")
+    fontspan3 = re.compile(r"class='(\w+)+-(\w+)+\snormal'\sstyle='font-feature-settings:\s\"(\w+\d*)\"\s(\d+),\s\"(\w+\d*)\"\s(\d+),\s\"(\w+\d*)\"\s(\d+)'>")
+    temptext = fontspan1.sub(r"class='\1-\3-\4-\2 normal'>", temptext)
+    temptext = fontspan2.sub(r"class='\1-\3-\4-\5-\6-\2 normal'>", temptext)
+    temptext = fontspan3.sub(r"class='\1-\3-\4-\5-\6-\7-\8-\2 normal'>", temptext)
 
     # add new font sortcodes for added classes
     for c in classes:
