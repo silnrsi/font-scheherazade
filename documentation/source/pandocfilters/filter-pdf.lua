69a18785bc0558685547f0b87800844324cc0ff4
BobH <bob_hallissy@sil.org>
2023-11-13 14:42:32 -0700

    Update lua filter so external links work in PDFs [nobuild]

--- documentation/source/pandocfilters/filter-pdf.lua
+++ documentation/source/pandocfilters/filter-pdf.lua
@@ -1,8 +1,13 @@
 function Link(el)
-  --el.target = string.gsub(el.target, "(.+)", "../../pdf/%0")
-  el.target = string.gsub(el.target, "(.+)", "")
-  el.target = string.gsub(el.target, "%.md", ".pdf")
-  el.target = string.gsub(el.target, "%.rawmd", ".md")
+  if string.find(el.target, "^https?://") == nil then
+    -- While full external links work fine, WeasyPrint doesn't 
+    -- seem capable of creating relative links within PDF files, 
+    -- and thus the resulting PDF files won't work if they are 
+    -- downloaded to a different folder. Until WeasyPrint is fixed,
+    -- we remove the target from relative links, making them 
+    -- nonfunctioning (even though they remain formatted as a link):
+    el.target = ""
+  end
   return el
 end
 
