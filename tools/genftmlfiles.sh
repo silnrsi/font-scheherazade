81ece757b8f7e42a854357b142ac4a204680d129
BobH <bob_hallissy@sil.org>
2024-03-27 11:28:38 -0600

    genftmlfiles.sh: Organize common parameters into an array [nobuild]

--- tools/genftmlfiles.sh
+++ tools/genftmlfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script rebuilds the algorithmically-generated ftml files. See README.md
 
@@ -18,18 +18,29 @@ fi
 prevfont="references/v4.000/ScheherazadeNew-Regular.ttf"
 prevver="4.0"
 
+commonParams=( \
+	--prevfont "$prevfont"  \
+	-s "url(../$prevfont)|$prevver"  \
+	--ap '_?dia[AB]$'  \
+	--xsl ../tools/ftml.xsl  \
+	--scale 200  \
+	-i source/glyph_data.csv  \
+	--langs 'sd,ur,ku,rhg,ky,wo'  \
+	-w 75% --ucdxml source/additional_ucd.xml  \
+	-s "url(../references/ScheherazadeNew-Regular.ttf)|ref"  \
+	-s "url(../results/ScheherazadeNew-Regular.ttf)|Reg" \
+)
+
 echo "Rebuilding ftml files..."
-tools/absgenftml.py -q -t 'AllChars (auto)'                      source/masters/ScheherazadeNew-Regular.ufo  tests/AllChars-auto.ftml        -l tests/logs/AllChars.log         --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Medium.ttf)|Med' -s 'url(../results/ScheherazadeNew-SemiBold.ttf)|SeBld'  -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
-tools/absgenftml.py -q -t 'AL Sorted (auto)'                     source/masters/ScheherazadeNew-Regular.ufo  tests/ALsorted-auto.ftml        -l tests/logs/ALsorted.log         --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
-tools/absgenftml.py -q -t 'Diac (auto)'                          source/masters/ScheherazadeNew-Regular.ufo  tests/Diac-auto.ftml            -l tests/logs/DiacTest1.log        --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' & 
-tools/absgenftml.py -q -t 'Diac Short (auto)'                    source/masters/ScheherazadeNew-Regular.ufo  tests/Diac-short-auto.ftml      -l tests/logs/DiacTest1-short.log  --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
-tools/absgenftml.py -q -t 'Subtending Marks (auto)'              source/masters/ScheherazadeNew-Regular.ufo  tests/SubtendingMarks-auto.ftml -l tests/logs/Subtending.log       --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
-tools/absgenftml.py -q -t 'DaggerAlef (auto)'                    source/masters/ScheherazadeNew-Regular.ufo  tests/DaggerAlef-auto.ftml      -l tests/logs/DaggerAlef.log       --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
-tools/absgenftml.py -q -t 'Kerning (auto)'                       source/masters/ScheherazadeNew-Regular.ufo  tests/Kern-auto.ftml            -l tests/logs/Kerning.log          --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Medium.ttf)|Med' -s 'url(../results/ScheherazadeNew-SemiBold.ttf)|SeBld'  -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
-tools/absgenftml.py -q -t 'Yehbarree (auto)'                     source/masters/ScheherazadeNew-Regular.ufo  tests/Yehbarree-auto.ftml       -l tests/logs/Yehbarree.log        --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
-tools/absgenftml.py -q -t 'Feature-Language Interactions (auto)' source/masters/ScheherazadeNew-Regular.ufo  tests/FeatLang-auto.ftml        -l tests/logs/FeatLang.log         --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s 'url(../references/ScheherazadeNew-Regular.ttf)|ref' -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg'  &
+tools/absgenftml.py -q -t 'AllChars (auto)'                      source/masters/ScheherazadeNew-Regular.ufo  tests/AllChars-auto.ftml        -l tests/logs/AllChars.log         "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Medium.ttf)|Med' -s 'url(../results/ScheherazadeNew-SemiBold.ttf)|SeBld'  -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
+tools/absgenftml.py -q -t 'AL Sorted (auto)'                     source/masters/ScheherazadeNew-Regular.ufo  tests/ALsorted-auto.ftml        -l tests/logs/ALsorted.log         "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
+tools/absgenftml.py -q -t 'Diac (auto)'                          source/masters/ScheherazadeNew-Regular.ufo  tests/Diac-auto.ftml            -l tests/logs/DiacTest1.log        "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' & 
+tools/absgenftml.py -q -t 'Diac Short (auto)'                    source/masters/ScheherazadeNew-Regular.ufo  tests/Diac-short-auto.ftml      -l tests/logs/DiacTest1-short.log  "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
+tools/absgenftml.py -q -t 'Subtending Marks (auto)'              source/masters/ScheherazadeNew-Regular.ufo  tests/SubtendingMarks-auto.ftml -l tests/logs/Subtending.log       "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
+tools/absgenftml.py -q -t 'DaggerAlef (auto)'                    source/masters/ScheherazadeNew-Regular.ufo  tests/DaggerAlef-auto.ftml      -l tests/logs/DaggerAlef.log       "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
+tools/absgenftml.py -q -t 'Kerning (auto)'                       source/masters/ScheherazadeNew-Regular.ufo  tests/Kern-auto.ftml            -l tests/logs/Kerning.log          "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Medium.ttf)|Med' -s 'url(../results/ScheherazadeNew-SemiBold.ttf)|SeBld'  -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
+tools/absgenftml.py -q -t 'Yehbarree (auto)'                     source/masters/ScheherazadeNew-Regular.ufo  tests/Yehbarree-auto.ftml       -l tests/logs/Yehbarree.log        "${commonParams[@]}" -s 'url(../results/ScheherazadeNew-Bold.ttf)|Bold' &
+tools/absgenftml.py -q -t 'Feature-Language Interactions (auto)' source/masters/ScheherazadeNew-Regular.ufo  tests/FeatLang-auto.ftml        -l tests/logs/FeatLang.log         "${commonParams[@]}"  &
 
 wait
 echo done.
-
-# tools/absgenftml.py -q -t 'AllChars (auto)' source/masters/ScheherazadeNew-Regular.ufo  tests/AllChars-auto.ftml -l tests/logs/AllChars.log   -s "url(../references/ScheherazadeNew-Regular.ttf)|ref" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 200 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% -s 'url(../results/ScheherazadeNew-Regular.ttf)|Reg' 
