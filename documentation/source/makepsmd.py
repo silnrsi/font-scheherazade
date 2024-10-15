a15636235c2cd0a07faf5fe1e2824fbc0983ad6d
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-10-15 12:14:17 -0500

    Update to use fontdocs files. [nobuild]

--- documentation/source/makepsmd.py
+++ documentation/source/makepsmd.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 '''Process font documentation .md files for use on product sites '''
-__url__ = 'http://github.com/silnrsi/fontdocs'
+__url__ = 'https://github.com/silnrsi/fontdocs'
 __copyright__ = 'Copyright (c) 2021-2024 SIL Global (https://www.sil.org)'
-__license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
+__license__ = 'Released under the MIT License (https://opensource.org/licenses/MIT)'
 __author__ = 'Victor Gaultney'
 
 import re
